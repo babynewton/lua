6e9272405bc4bb2d01818152ae2bc094b6789a7e
babynewton <babynewton73@gmail.com>
2014-09-04 17:59:24 +0900

    Closure was actually LClosure

--- src/lparser.cc
+++ src/lparser.cc
@@ -1619,11 +1619,11 @@ Closure *luaY_parser (lua_State *L, ZIO *z, Mbuffer *buff,
                       Dyndata *dyd, const char *name, int firstchar) {
   LexState lexstate;
   FuncState funcstate;
-  Closure *cl = luaF_newLclosure(L, 1);  /* create main closure */
+  LClosure *cl = luaF_newLclosure(L, 1);  /* create main closure */
   /* anchor closure (to avoid being collected) */
   setclLvalue(L, L->top, cl);
   incr_top(L);
-  funcstate.f = ((LClosure*)cl)->p = luaF_newproto(L);
+  funcstate.f = cl->p = luaF_newproto(L);
   funcstate.f->source = luaS_new(L, name);  /* create and anchor TString */
   lexstate.buff = buff;
   lexstate.dyd = dyd;
